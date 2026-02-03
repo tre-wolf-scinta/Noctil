93079200f805749fb7283e97e4556ce667f6c1b8
Tre-Wolf-Scinta <tre.scinta90@gmail.com>
2026-02-03 16:57:57 -0600

    Build: Initialize C++20 toolchain with CMake, Ninja, and vcpkg

---
+++ include/noctil/utils/UniqueFD.hpp
@@ -0,0 +1,8 @@
+// Constructor: Takes ownership of raw Linux integer (file descriptor)
+
+// Destructor: Promises to call close() on the integer when it goes out of scope
+
+// Delete Copy Constructor: Cannot clone hardware resource
+
+// Move Constructor: Ownership of this resource may be transferred to another scope
+
